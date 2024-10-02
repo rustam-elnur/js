d53b4532366282a1ad7f565e952df4e503218d46
rustam-elnur <elnurrustmov7@gmail.com>
2024-10-02 07:23:30 +0400

    enver bey kodlari yazdim lakin html de eks olmadi

---
+++ yeni.js
@@ -0,0 +1,76 @@
+// Statik yox Dinamik proqramlasdirma dilir 
+// menimsetme
+// verilen data
+
+// verilenin tipleri 
+// data struktor (melumatlar nece yerlesmelidi onu gosterir)
+// proqramlasdirma = "21"
+// etikhaking = 91
+
+
+// console.log("81" +91)  //172
+// toplama deyil stringlerin birlesmesidir 
+
+// verilenlerin tipleri 
+// transliator var birde
+// dildir kompliator hamsin tercume edin 
+// interpritator kesib her setri analiz edir
+
+// string ''  "" ` arasindadirsa olur metinlere aiddir 
+// deyisen - dirnaqda yazilmir
+
+telebeninAdi = "Elmar"
+telebeninYasi = 20
+telebeninEvlilikStatusu = false
+telebeninKursGostericisi = null //hele bitmeyib 
+SMMPlani = undefined //is getmir 
+// object tipi en onemlisi {}
+// [] array massiv matris riyaziyyat dili 
+
+// verilenlerin kompleks tipleri 
+birincitelebe = {  //key:value
+    ad: "Elmar",
+    soyad: "Misirzada",
+    yas: 20,
+    isEducated: true,
+} //obyekt
+
+ikincitelebe = {
+    ad: "Elgun",
+    soyad: "Resulov",
+    yas: 20,
+    iseducated: true,
+}
+
+telebeler = [
+    {
+    ad: "Elmar",
+    soyad: "Misirzada",
+    yas: 20,
+    isEducated: true,
+} , 
+{
+    ad: "Elgun",
+    soyad: "Resulov",
+    yas: 20,
+    iseducated: true,
+}
+
+]
+
+// console.log(telebeninAdi, telebeninYasi)
+
+var ModelTdTeqi = document.getElementById("ad")
+var soyadTdTeqi = document.getElementById(soyad)
+var yasTdTeqi = document.getElementById(yas)
+var iseducatedTdTeqi = document.getElementById(iseducated)
+
+
+console.log(ModelTdTeqi)
+
+
+adTdTeqi.innerText = "Elmar"
+soyadTdTeqi.innerText = "Misirzada"
+yasTdTeqi.innerText = 20
+iseducatedTdTeqi = true
+
